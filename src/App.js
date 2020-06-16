bd59bebaad15e79b8cc21773c9a5f4d5aa890fb7
MichalR <mrudny@gmail.com>
2020-06-16 18:33:00 +0200

    face recognition works

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import FaceRecognition from "./components/FaceRecognition/FaceRecognition"
 
 
 const app = new Clarifai.App({
-  apiKey: 'process.env.REACT_APP_CLARIFAI_API_KEY'
+  apiKey: 'a1bfee9623fe479f8bbeba9e6c142ca9'
 });
 
 const particlesOptions = {
@@ -30,8 +30,28 @@ class App extends Component {
     super();
     this.state = {
       input: "",
-      imageUrl:''
+      imageUrl:'',
+      box: {}
     };
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    
+    this.setState({box: box});
   }
 
   onInputChange = (e) => {
@@ -44,14 +64,8 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);        
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
   };
 
   render() {
@@ -65,7 +79,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
